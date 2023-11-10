8aab0bb5f9fabf2ab1dacf9301c18ea54495a9f3
fayezCoder <fayez.coder@gmail.com>
2023-11-10 06:47:18 +0300

    first committed

---
+++ android/app/src/main/kotlin/com/fayezcoder/storesx/storesx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fayezcoder.storesx.storesx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp( const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: SafeArea(child:WebView(
+        initialUrl: "https://fayezcoder.com",
+
+        javascriptMode: JavascriptMode.unrestricted,
+      )
+      ),
+    ),
+  ));
+}
+
+
